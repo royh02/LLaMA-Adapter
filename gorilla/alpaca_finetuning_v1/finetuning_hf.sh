3e6a4d9f4e12754f5c969d6bb7e0c9ee0e3039f3
Roy Huang <roy.huang@freddie.Millennium.Berkeley.EDU>
2023-10-15 17:00:05 -0700

    testing for gorilla

--- gorilla/alpaca_finetuning_v1/finetuning_hf.sh
+++ gorilla/alpaca_finetuning_v1/finetuning_hf.sh
@@ -1,13 +1,13 @@
-llama_path="/data1/llma/7B"
+llama_path="/data/roy.huang/llama/llama-2-7b"
 
-torchrun --nproc_per_node 2 --master_port=29501 finetuning.py \
+torchrun --nproc_per_node 5 --master_port=29501 finetuning.py \
     --model Llama7B_adapter \
     --llama_model_path "$llama_path"/ \
     --data_path ../gorilla-main/data/apibench/huggingface_train.json \
-    --adapter_layer 30 \
-    --adapter_len 10 \
+    --adapter_layer 25 \
+    --adapter_len 8 \
     --max_seq_len 512 \
-    --batch_size 4 \
+    --batch_size 2 \
     --epochs 10 \
     --warmup_epochs 2 \
     --blr 8e-1 \
